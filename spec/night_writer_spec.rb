ede8145cdccb0a6ac2a69482ed8c0002bb55ba91
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-22 15:38:08 -0600

    feat: add testing for to_braille method

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe NightWriter do
       expect(night_writer.translate_content).to eq("Created 'braille.txt' containing 12 characters")
     end
   end
+
+  describe '#to_braille' do
+    it 'takes in file with English and returns file with Braille characters' do
+      expect(night_writer.to_braille('h')).to eq("0.\n00\n..")
+      expect(night_writer.to_braille('a')).to eq("0.\n..\n..")
+    end
+  end
 end
