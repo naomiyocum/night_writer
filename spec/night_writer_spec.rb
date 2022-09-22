180b195d5588f0a32ff42e56ce0dc0e775e5b571
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-22 15:05:03 -0600

    feat: create translate_content tests/mock, and method

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,3 +1,18 @@
+require './lib/night_writer'
+
 RSpec.describe NightWriter do
-  
+  let(:night_writer) {NightWriter.new}
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(night_writer).to be_a(NightWriter)
+    end
+  end
+
+  describe '#translate_content' do
+    it 'reads CLI input and returns a line' do
+      allow(night_writer).to receive(:translate_content).and_return("Created 'braille.txt' containing 12 characters")
+      expect(night_writer.translate_content).to eq("Created 'braille.txt' containing 12 characters")
+    end
+  end
 end
