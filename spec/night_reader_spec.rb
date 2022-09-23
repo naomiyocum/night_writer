ed3cfc45629d062d78236d12ea2cc054be5f2562
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-23 08:35:57 -0600

    feat: create initialize and translate_content testing for night_reader

---
+++ spec/night_reader_spec.rb
@@ -0,0 +1,18 @@
+require './lib/night_reader'
+
+RSpec.describe NightReader do
+  let(:nightreader) {NightReader.new}
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(nightreader).to be_a(NightReader)
+    end
+  end
+  
+  describe '#translate_content' do
+    it 'reads CLI input and returns a line' do
+      allow(night_reader).to receive(:translate_content).and_return("Created 'original_message.txt' containing 256 characters")
+      expect(night_reader.translate_content).to eq("Created 'original_message.txt' containing 256 characters")
+    end
+  end
+end
