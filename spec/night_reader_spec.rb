2c8af9b9db4766dbadf04a5a41259cfc4e587582
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-23 21:42:56 -0600

    fix: adjust testing to fit with new spec_files dir

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -11,16 +11,16 @@ RSpec.describe NightReader do
   
   describe '#read_content' do
     it 'reads content from the file' do
-      allow(night_reader).to receive(:filename).and_return('./testing_braille.txt')
+      allow(night_reader).to receive(:filename).and_return('./spec/spec_files/testing_braille.txt')
       expect(night_reader.read_content).to eq("000.0.\n...0.0\n0.0.0.")
     end
   end
   
   describe '#write_content' do
     it 'writes translated content' do
-      allow(night_reader).to receive(:filename).and_return('testing_braille.txt')
-      allow(night_reader).to receive(:back_to_eng).and_return('testing_original_message.txt')
-      expect(night_reader.write_content).to eq("Created 'testing_original_message.txt' containing 20 characters")
+      allow(night_reader).to receive(:filename).and_return('./spec/spec_files/testing_braille.txt')
+      allow(night_reader).to receive(:back_to_eng).and_return('./spec/spec_files/testing_original_message.txt')
+      expect(night_reader.write_content).to eq("Created './spec/spec_files/testing_original_message.txt' containing 20 characters")
     end
   end
   
@@ -28,7 +28,7 @@ RSpec.describe NightReader do
     it 'translates from Braille to English' do
       expect(night_reader.to_english("0.\n.0\n00")).to eq('z')
       expect(night_reader.to_english("0.\n..\n0.")).to eq('k')
-      
+  
       expect(night_reader.to_english("000.0.\n...0.0\n0.0.0.")).to eq('moo')
     end
   end
