91be5caf31fba19bcd34623fc1e7be9a684b3ffa
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-23 21:40:12 -0600

    create: dir w testing files and adjust night_writer testing

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -11,19 +11,19 @@ RSpec.describe NightWriter do
   
   describe '#read_content' do
     it 'reads content from the file' do
-      allow(night_writer).to receive(:filename).and_return('./testing_file.txt')
+      allow(night_writer).to receive(:filename).and_return('./spec/spec_files/testing_file.txt')
       expect(night_writer.read_content).to eq("moo")
     end
   end
   
   describe '#write_content' do
     it 'writes translated content' do
-      allow(night_writer).to receive(:filename).and_return('./testing_file.txt')
-      allow(night_writer).to receive(:translated_file).and_return('testing_braille.txt')
-      expect(night_writer.write_content).to eq("Created 'testing_braille.txt' containing 3 characters")
+      allow(night_writer).to receive(:filename).and_return('./spec/spec_files/testing_file.txt')
+      allow(night_writer).to receive(:translated_file).and_return('./spec/spec_files/testing_braille.txt')
+      expect(night_writer.write_content).to eq("Created './spec/spec_files/testing_braille.txt' containing 3 characters")
     end
   end
-
+  
   describe '#to_braille' do
     it 'takes in file with English and returns file with Braille characters' do
       expect(night_writer.to_braille('t')).to eq(".0\n00\n0.")
@@ -32,7 +32,7 @@ RSpec.describe NightWriter do
       expect(night_writer.to_braille('i')).to eq(".0\n0.\n..")
       expect(night_writer.to_braille('n')).to eq("00\n.0\n0.")
       expect(night_writer.to_braille('g')).to eq("00\n00\n..")
-      
+  
       expect(night_writer.to_braille('turing')).to eq(".00.0..00000\n00..000..000\n0.000...0...")
     end
   end
